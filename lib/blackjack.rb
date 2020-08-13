a4cf296f9afd549aaaa82e169f385300c22b2d25
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-13 22:53:59 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,64 @@
+require 'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
+  
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  hit_or_stay = gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  return sum
 end
 
-def hit?
+def hit?(current_card_total)
   # code hit? here
+  again = ""
+  while again != 'no' do
+    prompt_user
+    answer = get_user_input
+    if answer == 'h'
+      current_card_total += deal_card
+      return current_card_total
+    elsif answer == 's'
+      again = 'no'
+      return current_card_total
+    else
+      invalid_command
+    end
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -40,5 +67,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  total = initial_round
+  while total < 22 do
+    total = hit?(total)
+    display_card_total(total)
+  end
+  end_game(total)
 end
-    
+
